470e9d305f3520de6072e1e7679d7421ef56c154
91i4ch <iosifov.p.v@gmail.com>
2020-09-01 15:53:20 +0300

    камера плавно следует за игроком

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] private GameObject player;
+    private float smoth = 0.5f;
+    private Vector3 velocity = Vector3.zero;
+    private Vector3 offset;
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = transform.position - player.transform.position;
+        transform.position = player.transform.position+offset;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    /// <summary>
+    /// LateUpdate is called every frame, if the Behaviour is enabled.
+    /// It is called after all Update functions have been called.
+    /// </summary>
+    void LateUpdate()
+    {
+        // Smoothly move the camera towards that target position
+        transform.position = Vector3.SmoothDamp(transform.position, player.transform.position+offset, ref velocity, smoth);
+    }
+}
