a48e19b60498399baaeab12306a4829af6f05d29
91i4ch <iosifov.p.v@gmail.com>
2020-09-23 10:50:06 +0300

    Update PlayerHealth.cs

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -11,7 +11,7 @@ public class PlayerHealth : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        health=MAXhealth;
     }
 
     // Update is called once per frame
