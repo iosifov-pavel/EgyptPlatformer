1da805ddf8b71e97a8f95333f812fcdccf7c537f
91i4ch <iosifov.p.v@gmail.com>
2020-10-17 00:08:07 +0300

    Spining Done

--- Assets/Pavel/Scripts/Platform_Spining.cs
+++ Assets/Pavel/Scripts/Platform_Spining.cs
@@ -4,27 +4,17 @@ using UnityEngine;
 
 public class Platform_Spining : MonoBehaviour
 {
-    public float maxAngle;
-    public float minAngle;
-    private float current;
-    private float speed = 6f;
+
+    private float speed = 126f;
     // Start is called before the first frame update
     void Start()
     {
-        current = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        float step = maxAngle*Time.deltaTime;
+        float step = speed*Time.deltaTime;
         transform.Rotate(new Vector3(0,0,step), Space.World);
-        current+=step;
-        if(current>=maxAngle){
-            float tmp = minAngle;
-            minAngle=maxAngle;
-            maxAngle=tmp;
-            current=0;
-        }
     }
 }
