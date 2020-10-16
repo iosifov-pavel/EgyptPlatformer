d45e464fda9d381bbc5cb311136fa2c1aaa6aee7
91i4ch <iosifov.p.v@gmail.com>
2020-10-16 10:36:41 +0300

    Правка для Ломающейся платформы

--- Assets/Pavel/Scripts/Platform_Fading.cs
+++ Assets/Pavel/Scripts/Platform_Fading.cs
@@ -18,7 +18,7 @@ public class Platform_Fading : MonoBehaviour
         
     }
 
-    private void OnCollisionEnter2D(Collision2D other) {
+    private void OnTriggerEnter2D(Collider2D other) {
         if(other.gameObject.tag=="Player"){
             StartCoroutine(Fade());
         }
