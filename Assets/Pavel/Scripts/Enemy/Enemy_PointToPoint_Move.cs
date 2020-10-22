21fc4e27ba00315464583c6e9380e1aa0746c59f
91i4ch <iosifov.p.v@gmail.com>
2020-10-22 13:02:26 +0300

    Летающий враг между двумя точками

--- Assets/Pavel/Scripts/Enemy/Enemy_PointToPoint_Move.cs
+++ Assets/Pavel/Scripts/Enemy/Enemy_PointToPoint_Move.cs
@@ -4,19 +4,29 @@ using UnityEngine;
 
 public class Enemy_PointToPoint_Move : MonoBehaviour
 {
-    Vector2 p1;
-    Vector2 p2;
-    int dir = 1;
+    [SerializeField] private Vector3 p1;
+    [SerializeField] private Vector3 p2;
     float speed = 2f;
     // Start is called before the first frame update
+
+    private void OnDrawGizmos() {
+        Gizmos.color=Color.red;
+        Gizmos.DrawLine(p1,p2);
+    }
     void Start()
     {
-        
+        p1 = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float step =  speed * Time.deltaTime; 
+        transform.position = Vector3.MoveTowards(transform.position, p2, step);
+        if(transform.position==p2){
+            Vector3 temp = p1;
+            p1 = p2;
+            p2 = temp;
+        }
     }
 }
