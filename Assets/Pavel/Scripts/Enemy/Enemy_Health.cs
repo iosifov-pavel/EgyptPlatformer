a59a0900df88f74fed72451e5d961dd0b19234bf
91i4ch <iosifov.p.v@gmail.com>
2020-11-05 18:25:46 +0300

    правки к урону ногами

--- Assets/Pavel/Scripts/Enemy/Enemy_Health.cs
+++ Assets/Pavel/Scripts/Enemy/Enemy_Health.cs
@@ -4,17 +4,19 @@ using UnityEngine;
 
 public class Enemy_Health : MonoBehaviour
 {
+    Enemy_Damage ed;
     public int health = 1;
     bool dead = false;
     // Start is called before the first frame update
     void Start()
     {
-        
+        ed = GetComponent<Enemy_Damage>();
     }
 
     // Update is called once per frame
 
     public void TakeDamage(int damage){
+        ed.isDamaged = true;
         health-=damage;
         if(health<=0){
             dead=true;
