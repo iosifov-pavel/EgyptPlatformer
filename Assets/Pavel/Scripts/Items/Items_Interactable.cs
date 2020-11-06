824ceaca3554c2ed31d6faa2a0742ef601b477d1
91i4ch <iosifov.p.v@gmail.com>
2020-11-06 17:01:51 +0300

    Смотреть описание
    -Правки для снарядов игрока
    - +кнопка для взаимодействия с объектами
    - +общий скрипт для активации взаимодействия

---
+++ Assets/Pavel/Scripts/Items/Items_Interactable.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Items_Interactable : MonoBehaviour
+{
+    [SerializeField] private GameObject button;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if(other.gameObject.tag=="Player"){
+            button.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other) {
+        if(other.gameObject.tag=="Player"){
+            button.SetActive(false);
+        }
+    }
+}
