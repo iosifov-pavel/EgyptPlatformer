79c0a2f67e401928e011df89f3670ccaf8f9b95f
Ivan <toxmon1996@gmail.com>
2020-11-11 22:35:32 +0300

    UI coins

--- Assets/Pavel/Scripts/Level/Level_Controller.cs
+++ Assets/Pavel/Scripts/Level/Level_Controller.cs
@@ -1,9 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Level_Controller : MonoBehaviour
 {
+    [SerializeField] private Text score;
+
+    int coins = 0;
+
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -15,4 +21,11 @@ public class Level_Controller : MonoBehaviour
     {
         
     }*/
+
+
+    public void Manager (int i)
+    {   
+        coins += i;
+        score.text = coins.ToString();
+    }
 }
