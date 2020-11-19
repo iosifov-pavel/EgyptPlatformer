17e541c2e83f2e8b5694e80151973e73d3fcb23c
91i4ch <iosifov.p.v@gmail.com>
2020-11-19 11:14:20 +0300

    Финальный вариант маятника

--- Assets/Pavel/Scripts/Traps/Trap_Sway.cs
+++ Assets/Pavel/Scripts/Traps/Trap_Sway.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Trap_Sway : MonoBehaviour
 {
-    [SerializeField] float base_speed = 40;
+    [SerializeField] float base_speed = 50;
     [SerializeField] float dop_speed = 0;
     float sway_angle = 50;
     float curr_angle;
@@ -25,24 +25,24 @@ public class Trap_Sway : MonoBehaviour
         if(!go) return;
         float percent = Mathf.Abs(curr_angle/(2*sway_angle)*100);
         if(percent<=50){
-            dop_speed=Mathf.Abs(base_speed*2*percent/100);
+            dop_speed=(base_speed*2*(2*percent-50)/100) + 15;
         } else{
-            dop_speed=Mathf.Abs(base_speed*2*(100-percent)/100);
-        }
+            dop_speed=(base_speed*2*(50-2*(percent-50))/100) + 15;
+        } 
         float step = dir*(base_speed+dop_speed)*Time.deltaTime;
         curr_angle+=step;
         if(Mathf.Abs(curr_angle)>=2*sway_angle){
             dir*=-1;
             curr_angle=0;
             dop_speed=0;
-            StartCoroutine(Wait());
+            //StartCoroutine(Wait());
         }
         transform.Rotate(0,0,step);
     }
 
     IEnumerator Wait(){
         go=false;
-        yield return new WaitForSeconds(0.11f);
+        yield return new WaitForSeconds(0.09f);
         go=true;
     }
 }
