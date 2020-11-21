9df42beebc8a12495f09c093358c37226081a5f2
91i4ch <iosifov.p.v@gmail.com>
2020-11-21 20:59:58 +0300

    правки

---
+++ Assets/Pavel/Scripts/Items/IInteractable.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IIntercatable
+{
+    // Start is called before the first frame update
+    void Use();
+}
