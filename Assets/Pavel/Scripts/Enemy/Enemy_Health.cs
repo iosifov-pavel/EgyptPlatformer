a78856f7cc904884359c380479d122513999b459
91i4ch <iosifov.p.v@gmail.com>
2020-11-23 22:25:00 +0300

    Базовая реакция врагов на урон

--- Assets/Pavel/Scripts/Enemy/Enemy_Health.cs
+++ Assets/Pavel/Scripts/Enemy/Enemy_Health.cs
@@ -4,20 +4,25 @@ using UnityEngine;
 
 public class Enemy_Health : MonoBehaviour
 {
-    Enemy_Damage ed;
     public int health = 1;
     bool dead = false;
+    bool is_damaged;
+    Color original;
+    SpriteRenderer sprite;
+    float time=0.3f;
+    float time_invincible=0.2f;
     // Start is called before the first frame update
-    void Start()
-    {
-        ed = GetComponent<Enemy_Damage>();
+    void Start(){
+        sprite=GetComponent<SpriteRenderer>();
+        original=sprite.color;
     }
 
     // Update is called once per frame
 
     public void TakeDamage(int damage){
-        ed.isDamaged = true;
+        if(is_damaged) return;
         health-=damage;
+        StartCoroutine(ReactToDamage());
         if(health<=0){
             dead=true;
             Death();
@@ -26,5 +31,13 @@ public class Enemy_Health : MonoBehaviour
 
     public void Death(){
         Destroy(this.gameObject);
+    }
+
+    IEnumerator ReactToDamage(){
+        is_damaged=true;
+        sprite.color=Color.red;
+        yield return new WaitForSeconds(time);
+        sprite.color=original;
+        is_damaged=false;
     }
 }
