b017b28671a66514a0ec1520abfeca54f3004d09
91i4ch <iosifov.p.v@gmail.com>
2020-11-26 14:57:57 +0300

    Открывающаяся по кнопке дверь

---
+++ Assets/Pavel/Scripts/Items/Childs/IChild.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IChild
+{
+    // Start is called before the first frame update
+    bool On {get;set;}
+    bool Done {get;set;}
+
+}
