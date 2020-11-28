e8c9ab8a65427612bb703d24c0e6a03f392fa7ad
91i4ch <iosifov.p.v@gmail.com>
2020-11-28 19:19:46 +0300

    правки

--- Assets/Pavel/Scripts/Player/Player_LegDamage.cs
+++ Assets/Pavel/Scripts/Player/Player_LegDamage.cs
@@ -6,13 +6,15 @@ public class Player_LegDamage : MonoBehaviour
 {
     // Start is called before the first frame update
 
-
+    Rigidbody2D rb2;
     // Update is called once per frame
 
 
     private void OnTriggerEnter2D(Collider2D other) {
         if(other.gameObject.tag=="Enemy"){
             other.gameObject.GetComponent<Enemy_Health>().TakeDamage(1);
+            rb2 = transform.parent.gameObject.GetComponent<Rigidbody2D>();
+            rb2.velocity = new Vector2(rb2.velocity.x,0);
             transform.parent.gameObject.GetComponent<Rigidbody2D>().AddForce(transform.up*12f, ForceMode2D.Impulse);
         }
     }
