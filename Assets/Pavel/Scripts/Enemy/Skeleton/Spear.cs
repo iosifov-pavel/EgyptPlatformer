6ecc3981ebe1ecd240fe690ce7091f0cec71d438
91i4ch <iosifov.p.v@gmail.com>
2020-11-30 18:19:31 +0300

    Копьё уничтожается через время и застревает в земле

--- Assets/Pavel/Scripts/Enemy/Skeleton/Spear.cs
+++ Assets/Pavel/Scripts/Enemy/Skeleton/Spear.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Spear : MonoBehaviour
 {
+    float lifetime = 8f;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,15 @@ public class Spear : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        lifetime-=Time.deltaTime;
+        if(lifetime<=0){
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if(other.gameObject.tag=="Ground"){
+            other.gameObject.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+        }
     }
 }
