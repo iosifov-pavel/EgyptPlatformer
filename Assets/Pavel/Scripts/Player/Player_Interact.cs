53ccdec29596dec7cccb079c5226bae3b1cea67c
91i4ch <iosifov.p.v@gmail.com>
2020-12-09 12:25:09 +0300

    Переписано взаимодействие с объектами по кнопке

--- Assets/Pavel/Scripts/Player/Player_Interact.cs
+++ Assets/Pavel/Scripts/Player/Player_Interact.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 
 public class Player_Interact : MonoBehaviour
 {
-    [SerializeField] private GameObject button;
+    //[SerializeField] private GameObject button;
+    GameObject UI;
+    GameObject button;
    // public bool interact = false;
     // Start is called before the first frame update
-
+    private void Start() {
+        UI = transform.parent.gameObject.GetComponent<Player_UIHolder>().UI;
+        button = UI.transform.GetChild(1).GetChild(3).gameObject;
+    }
 
     // Update is called once per frame
 
