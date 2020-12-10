b97021ee7467c65e57ee69d4cd1c0404f699d213
91i4ch <iosifov.p.v@gmail.com>
2020-12-10 12:47:23 +0300

    правим

--- Assets/Pavel/Scripts/Game/Manager_Level.cs
+++ Assets/Pavel/Scripts/Game/Manager_Level.cs
@@ -1,16 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Manager_Level : MonoBehaviour
 {
     [SerializeField] int level_id;
-    Level info;
+    public Level info;
+    string name_lvl;
 
     // Start is called before the first frame update
     void Start()
     {
-        info = new Level(level_id);
+        name_lvl = SceneManager.GetActiveScene().name;
+        info = new Level(level_id,name_lvl);
     }
 
     // Update is called once per frame
@@ -26,14 +29,14 @@ public class Level{
     public bool blocked;
     public float time;
     public int score;
-    public string name;
+    public string name="default";
 
-    public Level(int i){
+    public Level(int i,string s){
         id = i;
         complete = false;
         blocked = false;
         time = 0;
         score=0;
-        name = "default";
+        name = s;
     }
 }
