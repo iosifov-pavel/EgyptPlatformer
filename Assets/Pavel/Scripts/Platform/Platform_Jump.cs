b8d44535b49897c1bff26430720de9d88f017e1c
91i4ch <iosifov.p.v@gmail.com>
2020-12-13 18:59:55 +0300

    типа третий уровень

--- Assets/Pavel/Scripts/Platform/Platform_Jump.cs
+++ Assets/Pavel/Scripts/Platform/Platform_Jump.cs
@@ -13,7 +13,7 @@ public class Platform_Jump : MonoBehaviour
         if(other.gameObject.tag=="GroundCheck"){
         Rigidbody2D rb = other.attachedRigidbody;
         rb.velocity = new Vector2(rb.velocity.x/4,0);
-        rb.AddForce(transform.up*10f, ForceMode2D.Impulse);
+        rb.AddForce(transform.up*14f, ForceMode2D.Impulse);
         }
     }
 }
