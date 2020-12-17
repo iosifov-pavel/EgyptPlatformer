5c767a03875f900b0a0a7daeba0416a78283e550
91i4ch <iosifov.p.v@gmail.com>
2020-12-17 16:37:47 +0300

    правки

--- Assets/Pavel/Scripts/Items/Bonus_1UP.cs
+++ Assets/Pavel/Scripts/Items/Bonus_1UP.cs
@@ -6,6 +6,7 @@ public class Bonus_1UP : MonoBehaviour
 {
     // Start is called before the first frame update
     Player_Health player_Health;
+    bool yes=false;
     //Manager_Level level;
     void Start()
     {
@@ -19,9 +20,11 @@ public class Bonus_1UP : MonoBehaviour
     }
 
     private void OnTriggerEnter2D(Collider2D player) 
-    {
+    {   
+        if(yes) return;
         if (player.gameObject.tag == "Player")
         {
+            yes = true;
             player.gameObject.GetComponent<Player_Health>().OneUp();
             Destroy(gameObject);
         }
--- Assets/Pavel/Scripts/Items/Bonus_MaxHP.cs
+++ Assets/Pavel/Scripts/Items/Bonus_MaxHP.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Bonus_MaxHP : MonoBehaviour
 {
     // Start is called before the first frame update
+    bool yes=false;
     void Start()
     {
         
@@ -18,8 +19,10 @@ public class Bonus_MaxHP : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D player) 
     {
+        if(yes) return;
         if (player.gameObject.tag == "Player")
         {
+            yes = true;
             player.gameObject.GetComponent<Player_Health>().MaxHPPlus();
             Destroy(gameObject);
         }
