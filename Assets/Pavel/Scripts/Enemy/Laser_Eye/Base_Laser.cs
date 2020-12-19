2e23af720a609c928b5f512d1697b539653a6a7d
91i4ch <iosifov.p.v@gmail.com>
2020-12-19 19:39:14 +0300

    Базовый глаз лазер готов

--- Assets/Pavel/Scripts/Enemy/Laser_Eye/Base_Laser.cs
+++ Assets/Pavel/Scripts/Enemy/Laser_Eye/Base_Laser.cs
@@ -8,11 +8,26 @@ public class Base_Laser : MonoBehaviour
     Vector2 start,end;
     RaycastHit2D hit2D;
     LayerMask ground;
+    GameObject laser;
+    BoxCollider2D size;
+    Vector3 l_pos;
+    float dist;
+    LineRenderer line;
     //Vector2 dir;
     void Start()
     {
         start = transform.position;
         ground = LayerMask.GetMask("Ground");
+        line = GetComponent<LineRenderer>();
+        line.positionCount = 2;
+        line.SetPosition(0,start);
+        laser = transform.GetChild(0).gameObject;
+        size= laser.GetComponent<BoxCollider2D>();
+        //laser.AddComponent<BoxCollider2D>();
+        //laser = new GameObject();
+        //laser.name = "laser";
+        //laser.transform.parent=transform;
+        //laser.transform.localPosition=Vector3.zero;
         //dir = new Vector2(-1,0);
     }
 
@@ -22,7 +37,26 @@ public class Base_Laser : MonoBehaviour
         hit2D = Physics2D.Raycast(transform.position,transform.right*-1,20f,ground);
         if(hit2D.collider!=null){
             end = hit2D.point;
+            line.SetPosition(1,end);
             Debug.DrawLine(start,end,Color.red, 0.1f);
+            SetLaser();
         }
+        else {
+            line.SetPosition(1,start);
+            ResetLaser();
+        }
+    }
+
+
+    void SetLaser(){
+        l_pos = (end+start)/2;
+        float s = hit2D.distance / 0.3f;
+        laser.transform.position=l_pos;
+        size.size = new Vector2(s,1);
+    }
+
+    void ResetLaser(){
+        laser.transform.localPosition=Vector3.zero;
+        size.size = Vector2.zero;
     }
 }
