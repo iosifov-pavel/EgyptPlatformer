f9daef7bf9f7696780d22ddc607e69b1a0c5e7da
91i4ch <iosifov.p.v@gmail.com>
2020-12-24 16:02:42 +0300

    правки под новые спрайты

--- Assets/Pavel/Scripts/Traps/Traps_Shoot.cs
+++ Assets/Pavel/Scripts/Traps/Traps_Shoot.cs
@@ -5,12 +5,31 @@ using UnityEngine;
 public class Traps_Shoot : MonoBehaviour
 {
     [SerializeField] private GameObject prefab;
+    [SerializeField] Sprite idle;
+    [SerializeField] Sprite u_idle;
+    Transform up_c,r_c;
+    [SerializeField] bool up = false;
+    SpriteRenderer spriteRenderer;
     Trap_Bullet trap_Bullet;
     private GameObject bullet;
     bool can_attack = true;
-    float wait = 1.5f;
+    float wait = 1f;
     // Start is called before the first frame update
-
+    void Start(){
+        r_c = transform.GetChild(0);
+        up_c = transform.GetChild(1);
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        if(up){
+            spriteRenderer.sprite = u_idle;
+            up_c.gameObject.SetActive(true);
+            r_c.gameObject.SetActive(false);
+        }
+        else{
+            spriteRenderer.sprite = idle;
+            up_c.gameObject.SetActive(false);
+            r_c.gameObject.SetActive(true);
+        }
+    }
     // Update is called once per frame
     void Update()
     {
@@ -20,11 +39,13 @@ public class Traps_Shoot : MonoBehaviour
     }
 
     IEnumerator Attack(){
+        //spriteRenderer.sprite = work;
         can_attack = false;
         bullet = Instantiate(prefab) as GameObject;
         trap_Bullet = bullet.GetComponent<Trap_Bullet>();
-        bullet.transform.position =transform.position + new Vector3(0,0,1);
-        trap_Bullet.GetDirection(transform.right);
+        bullet.transform.position = transform.position + new Vector3(0,0,1);
+        if(up) trap_Bullet.GetDirection(transform.up);
+        else trap_Bullet.GetDirection(transform.right);
         yield return new WaitForSeconds(wait);
         can_attack = true;
     }
