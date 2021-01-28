3030b66e89ffa8230db8fae2d3950e28f9fbd31c
91i4ch <iosifov.p.v@gmail.com>
2021-01-28 10:27:39 +0300

    правки тестовой сцены

--- Assets/Pavel/Scripts/Platform/Instant_Death.cs
+++ Assets/Pavel/Scripts/Platform/Instant_Death.cs
@@ -9,12 +9,21 @@ public class Instant_Death : MonoBehaviour
     // Start is called before the first frame update
     // Update is called once per frame
 
-
-    private void OnTriggerEnter2D(Collider2D other) {
-            if(other.gameObject.tag=="Player"){
+    void KillEverything(Collider2D other){
+        if(other.gameObject.tag=="Player" && other.gameObject.layer==9 || other.gameObject.layer==10){
             ph = other.gameObject.GetComponent<Player_Health>();
             ph.superman=false;
             ph.ChangeHP(damage);
         }
+        else if(other.gameObject.tag=="WeakSpot"){
+            Destroy(other.transform.parent.gameObject);
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D other) {
+        KillEverything(other);
+    }
+    
+    private void OnTriggerStay2D(Collider2D other) {
+        KillEverything(other);
     }
 }
