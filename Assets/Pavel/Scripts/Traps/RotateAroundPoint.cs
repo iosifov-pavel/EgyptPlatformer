6b7d7a653e80eaa384fb3e4c9f7e064c9a3d521e
91i4ch <iosifov.p.v@gmail.com>
2021-02-01 11:23:33 +0300

    Пила на палке

---
+++ Assets/Pavel/Scripts/Traps/RotateAroundPoint.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateAroundPoint : MonoBehaviour
+{
+    [SerializeField] Transform point;
+    [SerializeField] float speed = 20f;
+    [SerializeField] int dir = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.RotateAround(point.position,new Vector3(0,0,1),speed*dir*Time.deltaTime);
+    }
+}
