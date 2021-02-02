0cb1d8fb00d17174ae19a8f4eae17e67fa1ae993
91i4ch <iosifov.p.v@gmail.com>
2021-02-02 11:11:38 +0300

    весы

--- Assets/Pavel/Scripts/Items/Libra.cs
+++ Assets/Pavel/Scripts/Items/Libra.cs
@@ -5,21 +5,24 @@ using UnityEngine;
 public class Libra : MonoBehaviour
 {
     // Start is called before the first frame update
-    Quaternion original, childOriginal;
-    Transform stick, l,r;
-    BoxCollider2D left,right;
+    Quaternion original, LOriginal, ROriginal;
+    Transform stick, L,R;
+    BoxCollider2D leftB,rightB;
     public bool leftContact=false, rightContact=false;
     Transform playerT = null;
+    Rigidbody2D stick_rb;
     [SerializeField]LayerMask player;
     void Start()
     {
         stick = transform.GetChild(0);
-        l = stick.GetChild(0);
-        r = stick.GetChild(1);
+        stick_rb = stick.gameObject.GetComponent<Rigidbody2D>();
+        L = transform.GetChild(1);
+        R = transform.GetChild(2);
         original = stick.rotation;
-        childOriginal = r.rotation;
-        left = l.GetComponent<BoxCollider2D>();
-        right = r.GetComponent<BoxCollider2D>();
+        LOriginal = L.rotation;
+        ROriginal = R.rotation;
+        leftB = L.GetComponent<BoxCollider2D>();
+        rightB = R.GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
@@ -27,22 +30,12 @@ public class Libra : MonoBehaviour
     {
         checkContacts();
         if(!leftContact && !rightContact){
-            stick.rotation = Quaternion.Lerp(stick.rotation,original,0.01f);
-            l.rotation = childOriginal; 
-            r.rotation = childOriginal; 
+            stick_rb.bodyType = RigidbodyType2D.Kinematic;
+            stick_rb.angularVelocity = 0;
+            stick.rotation = Quaternion.Lerp(stick.rotation,original,0.033f);
         }
-        else if(leftContact){
-            l.rotation = childOriginal; 
-            r.rotation = childOriginal;
-            
-            Quaternion newRotation = Quaternion.Euler(0,0,stick.rotation.z+60);
-            stick.rotation = Quaternion.Lerp(stick.rotation,newRotation,0.005f);
-        }
-        else if(rightContact){
-            l.rotation = childOriginal; 
-            r.rotation = childOriginal;
-            Quaternion newRotation = Quaternion.Euler(0,0,stick.rotation.z-60);
-            stick.rotation = Quaternion.Lerp(stick.rotation,newRotation,0.005f);
+        else if(leftContact || rightContact){
+            stick_rb.bodyType = RigidbodyType2D.Dynamic;
         }
     }
 
@@ -50,30 +43,33 @@ public class Libra : MonoBehaviour
         List<ContactPoint2D> left_c = new List<ContactPoint2D>();
         List<ContactPoint2D> right_c = new List<ContactPoint2D>();
         ContactFilter2D cf2d = new ContactFilter2D();
+        cf2d.useLayerMask=true;
         cf2d.layerMask = player;
-        left.GetContacts(cf2d,left_c);
-        right.GetContacts(cf2d,right_c);
-        if(playerT!=null){
-            //playerT.rotation = Quaternion.Euler(0,0,0);
-            playerT.parent=null;
-        } 
+        leftB.GetContacts(cf2d,left_c);
+        rightB.GetContacts(cf2d,right_c);
         leftContact = false;
         rightContact = false;
         foreach(ContactPoint2D point in left_c){
             if(point.collider!=null){
                 leftContact=true;
-                //playerT = point.collider.gameObject.transform;
-                //playerT.parent = l;
+                playerT = point.collider.gameObject.transform;
+                playerT.parent = L;
                 break;
             }
         }
         foreach(ContactPoint2D point in right_c){
             if(point.collider!=null){
                 rightContact=true;
-                //playerT = point.collider.gameObject.transform;
-                //playerT.parent = r;
+                playerT = point.collider.gameObject.transform;
+                playerT.parent = R;
                 break;
             }
+        }
+        if(!leftContact && !rightContact){
+            if(playerT!=null){
+                playerT.rotation = Quaternion.Euler(0,0,0);
+                playerT.parent=null;
+            } 
         }
     }
 }
