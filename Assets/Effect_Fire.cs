9f6e76238ea77fb63b4f0882dce9fb663b3aad82
Ivan <toxmon1996@gmail.com>
2021-02-15 22:00:11 +0300

    FireParticle,SettingsUI

---
+++ Assets/Effect_Fire.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Effect_Fire : MonoBehaviour
+{
+     bool letPlay = false;
+    //private bool playFire = true;
+    //float initialTime = 2f;
+    //[SerializeField] ParticleSystem FireParticle ;
+    [SerializeField] ParticleSystem FireParticle ;
+    // Start is called before the first frame update
+  
+    // Update is called once per frame
+    void Update()
+    {       
+        
+        letPlay = !letPlay;
+      if(letPlay)
+         {
+             if(FireParticle.isPlaying)
+             {
+                // FireParticle.gameObject.SetActive(false);//
+                 FireParticle.Stop();
+                 FireParticle.enableEmission = false;
+                 StartCoroutine (Timer ());
+                 //StartCoroutine (FireOn ());
+                 //FireOn ();
+                 letPlay = false;
+             }
+           //  if(!FireParticle.isPlaying)
+           //  {
+           //      FireParticle.Play(); 
+           //      FireParticle.enableEmission = true; 
+           //      //FireParticle.gameObject.SetActive(true);
+           //      StartCoroutine (Timer ());
+           //      //FireOn ();
+           //      letPlay = true;
+           // }
+         }else{
+            // if(FireParticle.isPlaying)
+            // {
+            //    // FireParticle.gameObject.SetActive(false);//
+            //     FireParticle.Stop();
+            //     FireParticle.enableEmission = false;
+            //     StartCoroutine (Timer ());
+            //     //StartCoroutine (FireOn ());
+            //     //FireOn ();
+            //     letPlay = false;
+            // }
+             if(!FireParticle.isPlaying)
+             {
+                 FireParticle.Play(); 
+                 FireParticle.enableEmission = true; 
+                 //FireParticle.gameObject.SetActive(true);
+                 StartCoroutine (Timer ());
+                 //FireOn ();
+                 letPlay = true;
+            }
+         }
+
+    }
+
+
+    private void FireOn ()
+    {
+        
+        if(letPlay == false)
+        {
+            letPlay = true;
+        }
+        else 
+        {
+            letPlay = false;
+        }
+        //letPlay = true;
+        //yield return new WaitForSeconds(5f);
+        
+       // yield return new WaitForSeconds(5f);
+       // FireParticle.Stop();
+    }
+
+    IEnumerator Timer ()
+    {
+        yield return new WaitForSeconds(10f);
+    }
+   // IEnumerator FireOff ()
+   // {
+   //    // yield return new WaitForSeconds(2f);
+   //    // FireParticle.Play();
+   //     yield return new WaitForSeconds(5f);
+   //     FireParticle.Stop();
+   // }
+
+ // public void play() 
+ // { 
+ //     FireParticle.Play(); 
+ //      FireParticle.enableEmission = true; 
+ //      }
+ // public void stop() {
+ //      FireParticle.enableEmission = false;
+ //       }
+}
---
+++ Assets/Pavel/Scripts/UI/Settings.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Settings : MonoBehaviour
+{
+
+    public GameObject PanelMenu;
+    public GameObject SettingsMenu;
+    public GameObject SoundBt;
+    public GameObject LanguageBt;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SettingsBtm()
+    {
+        Time.timeScale = 0f;
+        PanelMenu.SetActive(false);
+        SettingsMenu.SetActive(true);
+        
+    }
+
+    public void SoundBtm()
+    {
+        Time.timeScale = 0f;
+        LanguageBt.SetActive(false);
+        SoundBt.SetActive(true);
+    }
+
+    public void LanguageBtm()
+    {
+        Time.timeScale = 0f;
+        SoundBt.SetActive(false);
+        LanguageBt.SetActive(true);
+    }
+
+    public void BackBtm()
+    {
+        Time.timeScale = 0f;
+        SettingsMenu.SetActive(false);
+        PanelMenu.SetActive(true);
+       
+    }
+}
