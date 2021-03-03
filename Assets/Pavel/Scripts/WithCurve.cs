4027902fd47df515fdc2e1c7a70b4ba40ac4408e
91i4ch <iosifov.p.v@gmail.com>
2021-03-03 13:34:30 +0300

    скрипт движения по кривым с кучей плюшек

--- Assets/Pavel/Scripts/WithCurve.cs
+++ Assets/Pavel/Scripts/WithCurve.cs
@@ -6,46 +6,131 @@ public class WithCurve : MonoBehaviour
 {
     // Start is called before the first frame update
     [SerializeField] AnimationCurve parabola;
-    [SerializeField] [Range(10,100)] int precision=10;
+    [SerializeField] [Range(50,500)] int precision=10;
     [SerializeField] float multiplier = 1;
     [SerializeField] float speed = 2f;
-    [SerializeField] Vector3 a,b;
-    [SerializeField] int iterator = 0;
-    public float step;
-    public float value;
+    [SerializeField] Transform body;
+    [SerializeField] Transform start,end;
+    [SerializeField] bool endless = false;
+    [SerializeField] bool destroyAtTheEndPoint = false;
+    [SerializeField] bool continueStraightAfterEnd = false;
+    [SerializeField] bool goCircleBackward = false;
+    [SerializeField] bool backward = false;
+    [SerializeField] bool RotateToDirection = true;
+    [SerializeField] float delayAtPoints = 0f;
+    int iterator = 0;
+    float step;
+    float value;
     Vector3 prev_pos;
+    Vector3 rotate_direction;
+    Vector3 direction;
+    float distance;
+    Vector3 normal;
+    Vector3 result_point;
+    bool forward = true;
+    bool afterEnd = false;
+    bool cango = true;
     void Start()
     {
-        transform.position = a;
+        if(endless){
+            parabola.postWrapMode = WrapMode.Loop;
+            continueStraightAfterEnd = false;
+            destroyAtTheEndPoint = false;
+            goCircleBackward = false;
+            backward = false;
+        }
+        if(goCircleBackward) backward = false;
+        if(backward) goCircleBackward = false;
+        body.transform.position = start.position;
         step = 1f / (float)precision;
-        Debug.DrawLine(a,b,Color.red,1000f);
-        prev_pos = transform.position;
-        //iterator = 0;
+        Debug.DrawLine(start.position,end.position,Color.red,0.001f);
+        direction = (end.position - start.position).normalized;
+        distance = (end.position - start.position).magnitude;
+        normal = new Vector3(direction.y*-1,direction.x,0);
+        prev_pos = body.transform.position;
+        result_point = body.transform.position;
     }
 
     // Update is called once per frame
     void Update()
-    {
-        if(iterator<0) iterator=0;
-        if(iterator>precision) iterator = precision;
-        value = iterator*step;
-        float curve_value = parabola.Evaluate(value);
-        float result_c_value = curve_value*multiplier;
-        Debug.Log("C " +result_c_value);
-        Vector3 new_base_pos = Vector3.Lerp(a,b,value);
-        Debug.Log("B " +new_base_pos);
-        Vector3 direction = (b - a).normalized;
-        Vector3 norm = new Vector3(direction.y*-1,direction.x,0);
-        Debug.Log("N " +norm);
-        Vector3 result = new_base_pos + norm * result_c_value;
-        Debug.Log("R " +result);
-        transform.position = result;
-        Debug.DrawLine(transform.position, prev_pos, Color.green, 10f);
-        prev_pos = transform.position;
-        //Move();
+    {   
+        if(!cango) return;
+        Check();
+        Calculate();
+        Move();
     }
 
-    void Move(Vector2 normal, float curve_v){
+    void Check(){
+        if(body.transform.position == end.position){
+            if(destroyAtTheEndPoint) Destroy(gameObject);
+            if(endless){
+                start.position = end.position;
+                end.position += direction * distance;
+                iterator = 0;
+            }
+            if(continueStraightAfterEnd){
+                afterEnd = true;
+            }
+            if(goCircleBackward){
+                Vector3 temp = end.position;
+                end.position = start.position;
+                start.position = temp;
+                iterator = 0;
+            }
+            if(backward){
+                forward = false;
+            }
+            StartCoroutine(delay());
+        }
+        else if(body.transform.position == start.position){
+            if(backward){
+                forward = true;
+            }
+            StartCoroutine(delay());
+        }
+        if(body.transform.position == result_point) {
+            if(!endless && !backward && (iterator>=precision || iterator<0)) return;
+            else if(backward){
+                if(forward) iterator++;
+                else iterator--;
+            }
+            else iterator++;
+        }
+    }
 
+    IEnumerator delay(){
+        cango = false;
+        yield return new WaitForSeconds(delayAtPoints);
+        cango = true;
+    }
+
+    void Calculate(){
+        if(afterEnd) return;
+        float iterator_step = iterator*step;
+        float curve_value = parabola.Evaluate(iterator_step);
+        value = curve_value*multiplier;
+        Debug.Log("C " + value);
+        Debug.Log("N " + normal);
+        result_point = Vector3.Lerp(start.position,end.position,iterator_step);
+        result_point = result_point + normal * value;
+        Debug.Log("R " + result_point);
+        Debug.Log("R+N " + result_point);
+    }
+
+    void Move(){
+        if(prev_pos != body.transform.position){
+            Debug.DrawLine(body.transform.position, prev_pos, Color.green, 10f);
+            rotate_direction = body.transform.position - prev_pos;
+            if(RotateToDirection) body.transform.forward = rotate_direction;
+            prev_pos = body.transform.position;
+        }
+        if(afterEnd){
+            Vector3 after_end_direction = rotate_direction;
+            Vector3 new_position = body.transform.position + after_end_direction;
+            body.transform.position = Vector3.MoveTowards(body.transform.position,new_position,speed*Time.deltaTime);
+        }
+        else{
+            body.transform.position = Vector3.MoveTowards(body.transform.position,result_point,speed*Time.deltaTime);
+        }
     }
 }
