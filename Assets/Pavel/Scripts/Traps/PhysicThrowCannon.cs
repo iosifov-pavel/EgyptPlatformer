ee19a580a8c2bc462a0c6693eda968ebf4496955
91i4ch <iosifov.p.v@gmail.com>
2021-03-03 17:27:43 +0300

    пушка, стреляющая по дуге физически

---
+++ Assets/Pavel/Scripts/Traps/PhysicThrowCannon.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhysicThrowCannon : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] GameObject patron;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerStay2D(Collider2D other) {
+        if(other.gameObject.tag=="Player"){
+            GameObject new_patron = Instantiate(patron);
+            new_patron.transform.position = transform.position;
+            new_patron.SetActive(true);
+            float v = HardLines.GetForce(new_patron.transform,other.transform, 45f);
+            float dir = 1 * Mathf.Sign(other.transform.position.x-transform.position.x);
+            var rb = new_patron.GetComponent<Rigidbody2D>();
+            Vector2 move = new Vector2(dir,1);
+            move.Normalize();
+            rb.AddForce(move*v, ForceMode2D.Impulse);
+        }
+    }
+}
