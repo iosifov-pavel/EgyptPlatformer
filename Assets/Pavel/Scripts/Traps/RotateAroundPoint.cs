458bbf801e48ed19d9d6afca8696b582e54285f7
91i4ch <iosifov.p.v@gmail.com>
2021-03-16 12:46:30 +0300

    правки,  + маятник к вращению вокруг точки

--- Assets/Pavel/Scripts/Traps/RotateAroundPoint.cs
+++ Assets/Pavel/Scripts/Traps/RotateAroundPoint.cs
@@ -8,6 +8,9 @@ public class RotateAroundPoint : MonoBehaviour
     [SerializeField] float speed = 20f;
     [SerializeField] int dir = 1;
     [SerializeField] bool staticChildRotation = false;
+    [SerializeField] bool swing = false;
+    [SerializeField] float swingAngle = 90f;
+    float count=0;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,7 +20,18 @@ public class RotateAroundPoint : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.RotateAround(point.position,new Vector3(0,0,1),speed*dir*Time.deltaTime);
-        if(staticChildRotation) transform.rotation = Quaternion.Euler(0,0,0);
+        if(swing){
+            float angle = speed*dir*Time.deltaTime;
+            transform.RotateAround(point.position,new Vector3(0,0,1),speed*dir*Time.deltaTime);
+            count+=Mathf.Abs(angle);
+            if(count>=Mathf.Abs(swingAngle)){
+                count=0;
+                dir*=-1;
+            }
+        }
+        else{
+            transform.RotateAround(point.position,new Vector3(0,0,1),speed*dir*Time.deltaTime);
+            if(staticChildRotation) transform.rotation = Quaternion.Euler(0,0,0);
+        }
     }
 }
