4ab47e9be98eb1b0ad8fdaf54c99ab301d45b9f5
91i4ch <iosifov.p.v@gmail.com>
2021-03-19 13:12:18 +0300

    платформа конвейер готова

--- Assets/Pavel/Scripts/Platform/Platform_Conveyer.cs
+++ Assets/Pavel/Scripts/Platform/Platform_Conveyer.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class Platform_Conveyer : MonoBehaviour
 {
     [SerializeField] float speed = 1f;
-    [SerializeField] float forceTime = 0f;
+    [SerializeField] float forceTime = 0.5f;
+    [SerializeField] bool isImpulse = false;
     string tittle = "conveyer";
     [SerializeField] int dir = -1;
     Vector2 force;
@@ -15,22 +16,28 @@ public class Platform_Conveyer : MonoBehaviour
     {
     }
 
+    private void Update() {
+            force = new Vector2(speed*dir,0);
+    }
+
     // Update is called once per frame
 
     private void OnTriggerEnter2D(Collider2D other) {
         if(other.gameObject.tag=="GroundCheck"){
-            force = new Vector2(speed*dir,0);
             player = other.transform.parent.gameObject.GetComponent<Movement>();
-            //player.SetOtherSource(tittle,force,-1);
+            if(!isImpulse) player.SetConstantForce(tittle,force);
         }
     }
 
     private void OnTriggerStay2D(Collider2D other) {
         if(other.gameObject.tag=="GroundCheck"){
+            if(isImpulse) player.SetImpulseForce(force,forceTime);
             //player.AddForces(force);
         }
     }
     private void OnTriggerExit2D(Collider2D other) {
-        //player.ResetOtherSource(tittle);
+        if(other.gameObject.tag=="GroundCheck"){
+            if(!isImpulse) player.RemoveForce(tittle);
+        }
     }
 }
